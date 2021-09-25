8358e02558f30950048b7523e9887e2b433fe2f9
Andrius <andriuss0316@gmail.com>
2021-09-25 12:13:29 +0300

    20210925

--- main.js
+++ main.js
@@ -2,10 +2,13 @@
 let rez = 0;
 let random = 0;
 
+let toggleNumber = 2;
 
 
 const push = document.querySelector('.push');
 const push2 = document.querySelector('.push2');
+const switchh = document.querySelector('.switch');
+const toggle = document.querySelector('.toggle');
 
 // skaicius.innerText 
 // number.innerText
@@ -16,6 +19,11 @@ const push2 = document.querySelector('.push2');
 { document.querySelector(".img").style.display = "none"; }
 
 { document.querySelector(".ispejimas3").style.display = "none"; }
+
+{ document.querySelector(".switch").style.display = "none"; }
+{ document.querySelector(".toggle").style.display = "none"; }
+
+
 
 
 function pushClick () {
@@ -28,9 +36,29 @@ function pushClick () {
 }
 
 function push2Click () {
+    { document.querySelector(".ispejimas3").style.display = "block"; }
+    { document.querySelector(".switch").style.display = "block"; }
+    { document.querySelector(".toggle").style.display = "block"; }
 
+    { document.querySelector(".push2").style.display = "none"; }
+    { document.querySelector(".ispejimas2").style.display = "none"; }
+    { document.querySelector(".img").style.display = "none"; }
+
+}
+
+function switchhClick () {
+    toggle.innerText = "on";
+
+
+    setTimeout(function(){
+        { document.querySelector(".switch").style.display = "none"; }
+        { document.querySelector(".toggle").style.display = "none"; }
+
+        { document.querySelector(".ispejimas3").style.display = "none"; }
+    }, 1000);
 }
 
 
 push.addEventListener('click', pushClick);
 push2.addEventListener('click', push2Click);
+switchh.addEventListener('click', switchhClick);
